d5e7dd6e38d59b9769bef722ec064b22730b9b58
AmeniJ <ajalali@exoplatform.com>
2016-05-05 16:50:35 +0100

    Add  the character  '_'

--- core/services/src/main/java/org/exoplatform/services/cms/impl/Utils.java
+++ core/services/src/main/java/org/exoplatform/services/cms/impl/Utils.java
@@ -657,7 +657,7 @@ public class Utils {
         }
         continue;
       }
-      if(i > 0 && !(Character.isLetterOrDigit(c) || c == '-' || c == '.')) {
+      if(i > 0 && !(Character.isLetterOrDigit(c) || c == '-' || c == '.' || c == '_')) {
         cleanedStr.deleteCharAt(i--);
         continue;
       }
